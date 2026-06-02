88afc60528fb6a0f86ced6b617a4afc089047e8b
Margarita Trejo <miss.trejooo@gmail.com>
2026-06-01 23:26:12 -0700

    Funcionalidad de Selector de archivo y almacenamiento

--- src/app/upload.ts
+++ src/app/upload.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatButtonModule, MatDividerModule, MatIconModule ],
   templateUrl: './upload.html',
   styleUrl: './upload.scss'
 })
 export class App {
+
+  //inicializar variable para almacenar archivo
+  file: File | null = null;
+
+  //Fn: guardar archivo seleccionado en variable : file
+  onFileSelected(event: any) {
+    this.file = event.target.files[0];
+    if (!this.file) {
+      console.log('No file selected');
+    }
+  }
 
 
 }
